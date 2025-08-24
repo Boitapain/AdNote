b9ef82f3ff85ea340458f41e963be4399b165f79
Vincent <vincentbullion@icloud.com>
2025-08-24 23:24:47 +0200

    Mise en production sur Render

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	preview: {
+        allowedHosts: ["adnote.onrender.com", "localhost", "127.0.0.1"]
+    }
 });
